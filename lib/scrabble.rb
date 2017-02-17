5840fd8bc778cb04186b8ffd29ae310317d3af3c
slague <stephanielague@gmail.com>
2017-02-17 12:05:43 -0700

    Friday work...

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,15 +1,17 @@
 class Scrabble
 
+
+
   def score(word)
     1
-# =======
     word.upcase.chars.map do |letter|
       point_values[letter]
     end.reduce(:+)
-# >>>>>>> 461e302fead492e9726ce495dd36b2aed66aa21c
+
   end
 
   def point_values
+    point_values =
     {
       "A"=>1, "B"=>3, "C"=>3, "D"=>2,
       "E"=>1, "F"=>4, "G"=>2, "H"=>4,
@@ -20,4 +22,40 @@ class Scrabble
       "Y"=>4, "Z"=>10
     }
   end
+
+
+
+  def score_with_multipliers(word, space_values, multiply_word_score=1)
+
+    extra_points = 0
+
+    to_double = space_values.each_index.select { |position| space_values[position] == 2}
+
+    to_triple = space_values.each_index.select { |position| space_values[position] == 3}
+
+    to_double.map! {|num| word[num]}
+    to_triple.map! {|num| word[num]}
+
+    extra_points += point_values.fetch(to_double[0].upcase)
+
+    #use "replace" to change the to_double array from letters to thier values from the hash??
+
+    #go through the values hash and find each letter from the double array add this value to the extra points variable
+    #go through the values hash and find each letter from the triple array and add this value TWICE to the extra points variable
+
+    if  multiply_word_score == 2 && space_values.count >= 7
+      10 + 2 * (score(word) + extra_points)
+    elsif double_word_score == 2
+      2 * (score(word) + extra_points)
+    else
+      score(word) + extra_points
+    end
+  end
+
+    # And there should be a 10-point bonus for any seven letter word before the word multiplier:
+
+
+
+
+
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -1,12 +1,29 @@
 gem 'minitest'
-require_relative '../lib/scrabble'
+require './lib/scrabble'
 require 'minitest/autorun'
-require 'minitest/emoji'
+require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
+
   def test_it_can_score_a_single_letter
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
+    assert_equal 8, Scrabble.new.score("hello")
   end
+
+  def test_it_can_score_with_multipliers
+    game = Scrabble.new
+    assert_equal 9, game.score_with_multipliers('hello', [1,2,1,1,1])
+    assert_equal 18, game.score_with_multipliers('hello', [1,2,1,1,1], 2)
+    assert_equal 58, game.score_with_multipliers('sparkle', [1,2,1,3,1,2,1], 2)
+
+  end
+
+
+  def
+    skip
+
+  end
+
 end
